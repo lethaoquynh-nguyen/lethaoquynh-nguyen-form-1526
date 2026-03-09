9226839b392d10efa7af59798f7f49111f017cb1
Quynh Nguyen <nguy1212@algonquinlive.com>
2026-03-09 14:40:37 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,68 @@
+// DECLARING VARIABLES
+const submitButton = document.querySelector('#submit-button');
+const fullNameInput = document.querySelector('#fullname');
+const emailInput = document.querySelector('#email');
+const messageInput = document.querySelector('#message');
+
+// Declare variable that will store regular expression for email
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+// READY TO START CODING
+function handleFormValidation() {
+    // START WITH INITIALLY 2 "EMPTY BUCKETS" (DATA/ERRORS)
+    const data = {};
+    const errors = []; // (guide-lines)
+    const errorsObj = {}; // (to match screenshot console format)
+
+    // FULL NAME
+    const fullname = fullNameInput.value.trim();
+    if (fullname !== '') {
+        data.fullname = fullname;
+    } else {
+        const msg = 'Full name is missing';
+        errors.push(msg);
+        errorsObj.fn = msg;
+    }
+
+    // EMAIL
+    const email = emailInput.value.trim();
+    if (email !== '') {
+        if (emailRegex.test(email)) {
+            data.email = email;
+        } else {
+            const msg = 'Email is not valid';
+            errors.push(msg);
+            errorsObj.em = msg;
+        }
+    } else {
+        const msg = 'Email is missing';
+        errors.push(msg);
+        errorsObj.em = msg;
+    }
+
+    // MESSAGE
+    const message = messageInput.value.trim();
+    if (message !== '') {
+        data.message = message;
+    } else {
+        const msg = 'Message name is missing'; // match screenshot text
+        errors.push(msg);
+        errorsObj.msg = msg;
+    }
+
+    // FEEDBACK/ERRORS
+    if (errors.length > 0) {
+        console.log('ERRORS', errorsObj);
+        console.log(errors);
+    } else {
+        console.log('COLLECTED DATA', data);
+
+        // Clear text-fields
+        fullNameInput.value = '';
+        emailInput.value = '';
+        messageInput.value = '';
+    }
+}
+
+// Register your form to "click" event.
+submitButton.addEventListener('click', handleFormValidation);
